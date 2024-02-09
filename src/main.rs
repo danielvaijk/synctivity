5abc2ed8166cb7140eacacbdb814ec7e173a1678
Daniel van Dijk <noreply+daniel@git.vandijk.sh>
2024-02-09 21:22:25 +0100

    refactor: reorganize the code into library modules
    Refs: #2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use clap::{Parser, ValueHint};
-use git2::{Commit, Repository};
-use regex::Regex;
+use email::EmailAddress;
 use std::path::Path;
+
+mod email;
+mod repo;
 
 #[derive(Parser)]
 struct Arguments {
@@ -15,98 +17,7 @@ struct Arguments {
 
     /// Your commit signature email addresses.
     #[arg(short, long, required = true, value_delimiter = ',', value_hint = ValueHint::EmailAddress)]
-    emails: Vec<String>,
-}
-
-fn is_email_valid(email: &String) -> bool {
-    match Regex::new(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$") {
-        Ok(regex) => regex.is_match(email),
-        Err(_) => panic!("Failed to create email validation regex."),
-    }
-}
-
-fn get_repositories_in_dir(dir: &Path) -> Vec<Repository> {
-    let mut repositories = Vec::new();
-
-    // If we are inside a Git repository already, then return that.
-    if dir.join(".git").is_dir() {
-        match Repository::open(dir) {
-            Ok(repository) => repositories.push(repository),
-            Err(error) => println!("failed to open repository: {}", error),
-        }
-
-        return repositories;
-    }
-
-    for entry in dir.read_dir().expect("Failed to read input directory.") {
-        let entry = match entry {
-            Ok(entry) => entry,
-            Err(error) => panic!("Failed to process directory entry: {}", error),
-        };
-
-        let git_path = Path::join(&entry.path(), ".git");
-
-        // Ignore entries that do not contain a .git directory.
-        if !Path::new(&git_path).is_dir() {
-            continue;
-        }
-
-        match Repository::open(entry.path()) {
-            Ok(repository) => repositories.push(repository),
-            Err(error) => println!("failed to open repository: {}", error),
-        }
-    }
-
-    repositories
-}
-
-fn get_commits_by_email<'repo>(
-    repository: &'repo Repository,
-    emails: &[String],
-) -> (u32, Vec<Commit<'repo>>) {
-    let mut walker = match repository.revwalk() {
-        Ok(value) => value,
-        Err(error) => panic!("failed to create revision walker: {}", error),
-    };
-
-    if let Err(error) = walker.push_head() {
-        panic!(
-            "failed to add HEAD commit to revision walker for traversal: {}",
-            error
-        );
-    }
-
-    let mut revision_count: u32 = 0;
-    let mut found_commits = Vec::new();
-
-    for revision in walker {
-        let oid = match revision {
-            Ok(value) => value,
-            Err(error) => panic!("failed to get OID from revision: {}", error),
-        };
-
-        let commit = match repository.find_commit(oid) {
-            Ok(value) => value,
-            Err(error) => panic!("failed to find commit with OID {}: {}", oid, error),
-        };
-
-        revision_count += 1;
-
-        let does_email_match = {
-            let commit_author = commit.author();
-            let commit_author_email = commit_author.email().unwrap_or("unknown");
-
-            emails.iter().any(|email| email == commit_author_email)
-        };
-
-        if !does_email_match {
-            continue;
-        }
-
-        found_commits.push(commit);
-    }
-
-    (revision_count, found_commits)
+    emails: Vec<EmailAddress>,
 }
 
 fn main() {
@@ -124,14 +35,8 @@ fn main() {
         panic!("Output directory is invalid.");
     }
 
-    for email in emails.iter() {
-        if !is_email_valid(&email) {
-            panic!("Email address '{email}' is invalid.")
-        }
-    }
-
-    for repo in get_repositories_in_dir(&input_dir) {
-        let (revision_count, found_commits) = get_commits_by_email(&repo, &emails);
+    for repo in repo::get_repositories_in_dir(&input_dir) {
+        let (revision_count, found_commits) = repo::get_commits_by_email(&repo, &emails);
 
         println!("revision count total: {revision_count}");
         println!("revision count matched: {}", found_commits.len());
