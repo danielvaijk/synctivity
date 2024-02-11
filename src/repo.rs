3a435348265bf19bcd4a1090433fb564d3af90ce
Daniel van Dijk <daniel@stentorian.io>
2024-02-11 02:35:00 +0100

    #3 Copy commits from loaded repositories to sync repo.

--- src/repo.rs
+++ src/repo.rs
@@ -1,6 +1,6 @@
 use crate::email::EmailAddress;
 use crate::SYNC_REPO_NAME;
-use git2::{Commit, Repository, RepositoryInitOptions};
+use git2::{Commit, Repository, RepositoryInitOptions, Sort, Tree};
 use std::io;
 use std::path::Path;
 use thiserror::Error;
@@ -19,8 +19,8 @@ pub fn read_all_in_dir(dir: &Path) -> Result<Vec<Repository>, RepoError> {
     let mut repositories = Vec::new();
 
     if is_dir_git_repo(&dir) {
-        let absolute_dir = dir.canonicalize()?;
-        let dir_name = absolute_dir.file_name().unwrap();
+        let dir_absolute = dir.canonicalize()?;
+        let dir_name = dir_absolute.file_name().unwrap();
 
         return if dir_name == SYNC_REPO_NAME {
             Err(RepoError::Validation(format!(
@@ -49,12 +49,13 @@ pub fn read_all_in_dir(dir: &Path) -> Result<Vec<Repository>, RepoError> {
     Ok(repositories)
 }
 
-pub fn get_commits_by_email<'repo>(
+pub fn get_all_commits_by_emails<'repo>(
     repository: &'repo Repository,
     emails: &[EmailAddress],
 ) -> Result<(u32, Vec<Commit<'repo>>), RepoError> {
     let mut revision_walker = repository.revwalk()?;
 
+    revision_walker.set_sorting(Sort::TOPOLOGICAL | Sort::REVERSE)?;
     revision_walker.push_head()?;
 
     let mut revision_count: u32 = 0;
@@ -94,6 +95,41 @@ pub fn read_or_create(path: &Path) -> Result<Repository, RepoError> {
     let options = options.initial_head("main");
 
     Ok(Repository::init_opts(path, &options)?)
+}
+
+pub fn create_empty_tree(repo: &Repository) -> Result<Tree, RepoError> {
+    let tree = repo.treebuilder(None)?.write()?;
+    let tree = repo.find_tree(tree)?;
+
+    Ok(tree)
+}
+
+pub fn copy_over_commits<'repo>(
+    repo: &'repo Repository,
+    parents: &mut Vec<Commit<'repo>>,
+    tree: &Tree,
+    commits: &Vec<Commit>,
+) -> Result<(), RepoError> {
+    for commit in commits {
+        let parents_ref: Vec<&Commit> = parents.iter().collect();
+
+        let message = commit.message().unwrap();
+        let signature = commit.author();
+
+        let commit_id = repo.commit(
+            Some("HEAD"),
+            &signature,
+            &signature,
+            message,
+            &tree,
+            &parents_ref,
+        )?;
+
+        parents.clear();
+        parents.push(repo.find_commit(commit_id)?);
+    }
+
+    Ok(())
 }
 
 fn is_dir_git_repo(dir: &Path) -> bool {
